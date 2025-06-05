6676342a49514cb4ab53a8d0f5a527f9962923ba
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-04 23:06:34 -0400

    Adiciona projeto Spring Boot inicializado pelo Spring Initializr

---
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/EmpregapiApplication.java
@@ -0,0 +1,13 @@
+package me.wendersonfarias.empregapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpregapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpregapiApplication.class, args);
+	}
+
+}
