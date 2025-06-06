2574c3a76cd8363569b72bca72a40804b707281a
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-06 17:04:33 -0400

    :sparkles: feat: adiciona configuração do Docker Compose e propriedades do PostgreSQL

--- empregapi/src/main/java/me/wendersonfarias/empregapi/EmpregapiApplication.java
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/EmpregapiApplication.java
@@ -1,13 +1,43 @@
 package me.wendersonfarias.empregapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import me.wendersonfarias.empregapi.dto.CandidatoRequest;
+import me.wendersonfarias.empregapi.dto.CandidatoResponse;
+import me.wendersonfarias.empregapi.model.Candidato;
+import me.wendersonfarias.empregapi.service.CandidatoService;
+
 @SpringBootApplication
-public class EmpregapiApplication {
+public class EmpregapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CandidatoService candidatoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpregapiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+
+		CandidatoRequest candidato = new CandidatoRequest();
+
+		candidato.setNomeCompleto("Wenderson Farias");
+		candidato.setEmail("wenderson@email.com");
+		candidato.setTelefone("123456789");
+		candidato.setEndereco("Rua Exemplo, 123");
+		candidato.setExperienciaProfissional("5 anos em desenvolvimento Java");
+		candidato.setHabilidades("Java, Spring Boot, SQL");
+		candidato.setHabilidades("ler e escrever");
+
+		// Salva o candidato no banco de dados
+		CandidatoResponse candidatoResponse = candidatoService.salvarCandidato(candidato);
+
+		// Exibe os dados do candidato salvo
+		System.out.println(candidatoResponse.toString());
+
+	}
 }
