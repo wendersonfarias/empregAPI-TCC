697dfa20a23f39d60d5a7220a18ef66a5e482c25
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-08 22:19:34 -0400

    :sparkles: feat: adiciona tratamento de exceção para CNPJ já cadastrado

---
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/exception/CnpjJaCadastradoException.java
@@ -0,0 +1,8 @@
+package me.wendersonfarias.empregapi.exception;
+
+public class CnpjJaCadastradoException extends RuntimeException {
+  public CnpjJaCadastradoException(String mensagem) {
+    super(mensagem);
+  }
+
+}
