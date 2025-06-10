92953f8fc0428f8a6b2601db858671edd2653611
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-09 22:24:55 -0400

    refactor(application): remove implementação de CommandLineRunner e código relacionado

--- empregapi/src/main/java/me/wendersonfarias/empregapi/EmpregapiApplication.java
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/EmpregapiApplication.java
@@ -1,19 +1,13 @@
 package me.wendersonfarias.empregapi;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpregapiApplication implements CommandLineRunner {
+public class EmpregapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpregapiApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
-
-		System.out.println("Iniciando aplicação...");
-	}
 }
