7b3e9e087df8885986442adf2b14e1432e275806
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-10 14:30:54 -0400

    feat(config): adiciona classes de configuração para perfis de desenvolvimento e teste

---
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/config/TestConfiguracao.java
@@ -0,0 +1,9 @@
+package me.wendersonfarias.empregapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfiguracao {
+}
