46b761477d81a054ad9304eff36cd08abc747d92
Wenderson Farias <wendersonpfarias@gmail.com>
2025-06-15 17:45:56 -0400

    feat(dto): adiciona construtores padrão à classe InscricaoDetalhesDTO e CandidatoSimplificadoDTO

--- empregapi/src/main/java/me/wendersonfarias/empregapi/dto/InscricaoDetalhesDTO.java
+++ empregapi/src/main/java/me/wendersonfarias/empregapi/dto/InscricaoDetalhesDTO.java
@@ -2,10 +2,14 @@ package me.wendersonfarias.empregapi.dto;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import me.wendersonfarias.empregapi.enumeracao.StatusInscricao;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class InscricaoDetalhesDTO {
   private Long numeroInscricao;
   private LocalDateTime dataInscricao;
@@ -13,6 +17,8 @@ public class InscricaoDetalhesDTO {
   private CandidatoSimplificadoDTO candidato;
 
   @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
   public static class CandidatoSimplificadoDTO {
     private Long id;
     private String nomeCompleto;
